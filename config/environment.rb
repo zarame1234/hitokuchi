2e3be8d4a5034cc2bef13aa2568e3ba14b21dbe3
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-03-04 05:16:03 +0000

    [Add]devise導入

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,3 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-const webpack = require('webpack')
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    Popper: 'popper.js'
-  })

0575cc15e2b0d894d5b800257e5b3ebfc36ccddf
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-03-05 03:06:56 +0000

    [Update]投稿機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,17 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.user.id = current_user.id
+    post.save
+    redirect_to root_path
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
@@ -10,4 +19,12 @@ class PostsController < ApplicationController
 
   def edit
   end
+
+  def desteoy
+  end
+
+  private 
+    def post_params
+      params.require(:post).permit(:shop_name, :body, :image)   
+    end
 end

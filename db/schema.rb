ebb2e2ec36b140330de61bc1baa8d3a8d0c34b3b
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-03-06 07:15:52 +0000

    [Update]usersテーブの修正

--- db/schema.rb
+++ db/schema.rb
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2025_03_04_081627) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.text "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true

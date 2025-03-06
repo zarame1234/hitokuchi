62ca961ed8394a4ba7d9dc4c78160d7feb71ae81
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-03-06 09:28:39 +0000

    [Update]user画面修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,17 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update 
+    @user = User(user_params)
+    @user.update
+    redirect_to user_path(@user)
+end
+
+private
+def user_params
+  params.require(:user).permit(:name, :introduction, :profile_imane)
+end
 end

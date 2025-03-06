c598f57e5b32fa5dd699675c4c29e908f1d48eb1
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-03-06 02:02:56 +0000

    [Add]userコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   resources :posts
+  resources :users, only: [:create, :show, :edit, :update, :destroy ]
   get '/about' => 'homes#about'
 end

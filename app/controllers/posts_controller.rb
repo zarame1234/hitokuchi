37647ac499b7fb6d60961180d1e5a09494ad5bae
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-03-12 10:37:56 +0000

    [Update}レイアウト修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     if @post.save
-      redirect_to post_path(@post)
+      redirect_to user_path(@post.user_id)
     else
       render :new
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
   before_action :ensure_guest_user, only: [:edit]
   
   def show
@@ -22,6 +23,13 @@ end
 private
 def user_params
   params.require(:user).permit(:name, :introduction, :profile_imane)
+end
+
+def is_matching_login_user
+  user = User.find(params[:id]) 
+  unless user.id == current_user.id
+    redirect_to posts_path
+  end
 end
 
 def ensure_guest_user

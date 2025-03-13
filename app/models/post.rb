0a69f7d17e9236aca73936070bfc2513810e233d
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-03-13 03:26:13 +0000

    [Update}レイアウト修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,11 +6,11 @@ class Post < ApplicationRecord
   validates :body,presence: true
   validates :image,presence: true
 
-  def get_image(width, height)
+  def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image.variant(resize_to_limit: [width, height]).processed
+    image
   end
 end

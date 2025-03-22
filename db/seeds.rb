6c0624478b00501635c5983a46e44442a6d2bc7d
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-03-22 02:28:14 +0000

    seedへサンプルを記入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+bob = User.find_or_create_by!(email: "bob@example.com") do |user|
+  user.name = "bob"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")
+end
+
+lucy = User.find_or_create_by!(email: "lucy@example.com") do |user|
+  user.name = "lucy"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")
+end
+
+lucas = User.find_or_create_by!(email: "lucas@example.com") do |user|
+  user.name = "lucas"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")
+end
+
+Post.find_or_create_by!(shop_name: "おでん屋台") do |post|
+  post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg")
+  post.body = "たまたま見つけた屋台。"
+  post.user = bob
+end
+
+Post.find_or_create_by!(shop_name: "洋菓子屋") do |post|
+  post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg")
+  post.body = "甘いものが食べたくなり購入"
+  post.user = lucy
+end
+
+Post.find_or_create_by!(shop_name: "ハンバーガー屋") do |post|
+  post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg")
+  post.body = '腹持ちがいい！'
+  post.user = lucas
+end

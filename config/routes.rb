3ddb3aa1e7705e9b28297a6bac56a0ffad8f78bc
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-03-23 13:03:34 +0000

    [Update]検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'searches/search'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   resources :posts
   resources :users, only: [:index, :create, :show, :edit, :update, :destroy ]
   get '/about' => 'homes#about'
+  get 'searches/search'
 
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"

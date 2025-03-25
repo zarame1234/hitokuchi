538b879dd07e6e5ab16d6a14e7a8a8a64bacea04
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-03-25 01:06:24 +0000

    [Update]コメント機能のレイアウト作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
-  resources :posts
-  resources :users, only: [:index, :create, :show, :edit, :update, :destroy ]
   get '/about' => 'homes#about'
   get 'searches/search'
+  resources :posts do
+    resources :post_comments, only: [:create]
+  end
+    
+  resources :users, only: [:index, :create, :show, :edit, :update, :destroy ]
 
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"

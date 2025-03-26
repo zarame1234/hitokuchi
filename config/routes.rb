5f20e44cff9b178e09285e5c24d7a95560c14daa
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-03-26 06:40:55 +0000

    [Update]エンドユーザーと管理者の構成を修正

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,9 @@ Rails.application.routes.draw do
       resources :post_comments, only: [:create, :destroy]
     end
     resources :users, only: [:index, :create, :show, :edit, :update, :destroy ]
-  end
-
-  devise_scope :user do
-    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+    devise_scope :user do
+      post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+    end
   end
 
   devise_for :admins, skip: [:registrations, :password], controllers: {

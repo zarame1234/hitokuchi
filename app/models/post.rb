6dc7e5c121543b83e131a5f6c5be9223ed5d94e9
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-03-27 08:48:02 +0000

    [Add]いいね機能実装のためのmodel作成・アソシエーション記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   
   validates :body,presence: true, length: { maximum: 50 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
  
   validates :name,presence: true
   validates :introduction, length: { maximum: 50 }

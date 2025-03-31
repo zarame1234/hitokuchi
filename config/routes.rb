e23c42df5e1ea4b2babf5798c09c1f06c9b9512c
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-03-31 11:39:39 +0000

    [Add]mapsコントローラー, routesの設定

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     devise_scope :user do
       post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
     end
+    resource :map, only: [:show]
   end
 
   devise_for :admins, skip: [:registrations, :password], controllers: {

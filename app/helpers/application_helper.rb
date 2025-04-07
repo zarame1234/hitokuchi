02af3f20bd2745e53b54323fb51b187c9051616b
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-04-07 11:21:58 +0000

    [Update]新規登録sippaiのエラーメッセージのレイアウト修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def devise_error_messages
+    return "" if resource.errors.empty?
+    html = ""
+    messages = resource.errors.full_messages.each do |msg|
+      html += <<-EOF
+        <div>
+          <li class="error_msg">#{msg}</li>
+         </div>
+      EOF
+    end
+    html.html_safe
+  end
 end

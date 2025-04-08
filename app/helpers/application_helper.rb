ebd498f945878ef054ea213d43f7ec4c2e74c37a
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-04-08 09:37:06 +0000

    [Update]新規登録失敗時のフォームサイズの修正/

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,20 @@
 module ApplicationHelper
+
   def devise_error_messages
     return "" if resource.errors.empty?
     html = ""
-    messages = resource.errors.full_messages.each do |msg|
-      html += <<-EOF
-        <div>
-          <li class="error_msg">#{msg}</li>
-         </div>
-      EOF
+
+    messages_order = ['name', 'email', 'password', ]
+    
+    messages_order.each do |field|
+      error_messages = resource.errors.full_messages_for(field)
+      error_messages.each do |msg|
+        html += <<-EOF
+          <div>
+            <li style="color: #930403;">#{msg}</li>
+           </div>
+        EOF
+      end
     end
     html.html_safe
   end
